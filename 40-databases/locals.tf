f0309a01310d5ff025bae5102a47c0ec1c4a1182
Jaya-Nagendra <nagendra.44@outlook.com>
2026-03-14 22:20:47 +0530

    roboshop-infra

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -11,4 +11,5 @@ locals {
   #database subnet in AZ 1a
   database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
   mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+  redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
 }
