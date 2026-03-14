cbf5d991107a50ba3deb13a89cf9f31f63958152
Jaya-Nagendra <nagendra.44@outlook.com>
2026-03-14 21:16:07 +0530

    roboshop-infra

---
+++ 40-databases/locals.tf
@@ -0,0 +1,14 @@
+locals {
+  ami_id = data.aws_ami.joindevops.id
+
+  common_tags = {
+    Project = var.project
+    Environment = var.environment
+    Terraform = "true"
+  }
+
+
+  #database subnet in AZ 1a
+  database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
+  mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+}
