c78a6b64e22caf3378d59ed2c72e4271b590d150
Jaya-Nagendra <nagendra.44@outlook.com>
2026-03-14 15:29:53 +0530

    roboshop-infra

---
+++ 10-sg/variables.tf
@@ -0,0 +1,21 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "sg_names" {
+  type = list
+  default = [
+    #database
+    "mongodb", "redis", "mysql", "rabbitmq",
+    #Backend
+    "catalogue", "user", "cart", "shipping", "payment",
+    "backend_alb",
+    "frontend",
+    "frontend_alb",
+    "bastian"
+  ]
+}
