87524bdaf3a576e9d0ada9280e716d96e1d8138c
Jaya-Nagendra <nagendra.44@outlook.com>
2026-03-18 11:10:34 +0530

    roboshop-infra

---
+++ 60-catalogue/main.tf
@@ -0,0 +1,50 @@
+resource "aws_instance" "catalogue" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.private_subnet_ids
+  vpc_security_group_ids = [local.catalogue_sg_id]
+
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-catalogue"
+    },
+    local.common_tags
+  )
+}
+
+resource "terraform_data" "catalogue" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+
+connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.catalogue.private_ip
+  }
+
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh catalogue dev"
+     ]
+  }
+}
+
+resource "aws_ec2_instance_state" "catalogue" {
+instance_id = aws_instance.catalogue.id
+state = "stopped"
+depends_on = [terraform_data.catalogue]
+}
+
+resource "aws_ami_from_instance" "catalogue" {
+  name               = "${var.project}-${var.environment}-catalogue"
+  source_instance_id = aws_instance.catalogue.id
+  depends_on = [aws_ec2_instance_state.catalogue]
+}
